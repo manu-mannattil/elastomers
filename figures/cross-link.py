e1ae6545280df40fa8d33a3720da6eb2d91ccd25
Manu Mannattil <manu.mannattil@posteo.net>
2025-04-06 13:23:05 +0300

    Plot G_N value for PDMS

--- figures/cross-link.py
+++ figures/cross-link.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
-
 """Young's modulus vs. Cross-linker concentration.
 
 This is a reproduction of Fig. S1 from Fernandez-Rico et al.  For some
 reason, these authors decided to use a log-log scale in their original
-plot, which makes the scaling less apparent.
+plot, which makes the scaling ambiguous.
 """
 
 import numpy as np
@@ -23,15 +22,29 @@ with plt.rc_context(rc):
 
     x, y = np.loadtxt("../experiments/figs1.dat", unpack=True)
 
-    # Make the coefficient matrix; note the transpose.
-    A = np.vstack([x[:-1], np.ones(len(x)-1)]).T
-    m, c = np.linalg.lstsq(A, y[:-1])[0]
-
     ax.plot(x, y, "ro")
-    ax.plot(x, m*x + c, "k--", zorder=-1000)
+    ax.plot(x, y, "wo", mfc="w", ms=7, zorder=-10)
+    ax.plot(x, y, "--", zorder=-20)
+    ax.plot(x, y, "w-", zorder=-30, lw=10)
 
-    ax.set_xlabel(r"cross-linker concentration (mol m$^{-3}$)")
-    ax.set_ylabel(r"Young's modulus (kPa)")
+    # Linear fit.
+    # A = np.vstack([x[:-1], np.ones(len(x) - 1)]).T
+    # m, c = np.linalg.lstsq(A, y[:-1])[0]
+    # ax.plot(x, m*x + c, "k--", zorder=-1000)
+
+    ax.set_xlabel(r"cross-linker concentration $\propto \nu$ (mol m$^{-3}$)")
+    ax.set_ylabel(r"$Y$ (kPa)")
+
+    ax.set_xlim(4.5, 13.5)
+    ax.plot([0, 15], [1.5 * 250, 1.5 * 250], "--", color="#666666", zorder=-100)
+
+    ax.annotate(r"\footnotesize $3G_\textsf{\tiny N}/2$ for PDMS (expected)", 
+                xy=(6, 370),
+                xytext=(7, 610),
+                ha="center", va="bottom",
+                color="#666666",
+                arrowprops=dict(arrowstyle="->", color="#999999", linewidth=0.5))
+
 
     plt.tight_layout()
     plt.savefig(
